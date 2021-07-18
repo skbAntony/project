f3e5e140bc199a38314e5c82164f1fe3e867d416
antony <stregat489@gmail.com>
2021-07-18 20:17:44 +0800

    新增index3.php

---
+++ index3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>範例接收資料</h1>
+    <?php 
+    $num = 123456;
+    echo $num;
+        // echo $_POST['first-name'].'<br>';
+        // echo $_POST['exampleInputEmail1'].'<br>'; 
+        // echo $_POST['recommend'].'<br>'; 
+        // if (isset($_POST['exampleCheck1'])) {
+        //     //已勾選
+        //     echo '123<br>';
+        // } else {
+        //     //未勾選
+        //     echo '456<br>';
+        // }
+    ?>
+</body>
+</html>
